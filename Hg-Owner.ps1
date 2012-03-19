a6b5135e717ed177413df550540b5009f753e91c
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2012-03-19 22:04:13 +0200

    Hg-Owner: pipelining support added

--- Hg-Owner.ps1
+++ Hg-Owner.ps1
@@ -1,10 +1,14 @@
-function Hg-Owner([string]$filename) {
-    hg log `
-        --no-merges `
-        --include $filename `
-        --template '{author}\n' `
-        | group `
-        | sort Count -Descending `
-        | %{$_.Name} `
-        | select -First 1 `
+function Hg-Owner([Parameter(ValueFromPipeline=$true)][string[]]$filename) {
+
+    Process {
+        hg log `
+            --no-merges `
+            --include $filename `
+            --template '{author}\n' `
+            | group `
+            | sort Count -Descending `
+            | %{$_.Name} `
+            | select -First 1 `
+    }
+
 }
