1fb5914d37acfd0cf32ef95ca186d371dd3e178a
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2012-03-21 20:53:02 +0200

    Hg-Owner: added algorithm which determines owner using annotate (not used yet)

--- Hg-Owner.ps1
+++ Hg-Owner.ps1
@@ -5,13 +5,25 @@ function Get-HgOwner([Parameter(Mandatory=$true, ValueFromPipeline=$true)] [stri
         function DetermineHgOwnerUsingLog([string] $Path) {
             hg log `
                 --no-merges `
-                --include $Path `
                 --template '{author}\n' `
+                $Path `
                 2> $null `
                 | group `
                 | sort Count -Descending `
                 | %{$_.Name} `
                 | select -First 1 `
+        }
+
+        function DetermineHgOwnerUsingAnnotate([string] $Path) {
+            hg annotate `
+                --user -v `
+                $Path `
+                2> $null `
+                | %{$_.split(':')[0].trim()} `
+                | group `
+                | sort Count -Descending `
+                | %{$_.Name} `
+                | select -First 1        
         }
 
     }
