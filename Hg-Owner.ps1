f785e6b4b3519fea7db44814d3f6ccbdbbef3385
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2012-03-21 20:46:19 +0200

    Hg-Owner: extracted algorithm to separate function for ability to add other algorithms

--- Hg-Owner.ps1
+++ Hg-Owner.ps1
@@ -1,15 +1,23 @@
 function Get-HgOwner([Parameter(Mandatory=$true, ValueFromPipeline=$true)] [string[]] $Path) {
 
+    Begin {
+
+        function DetermineHgOwnerUsingLog([string] $Path) {
+            hg log `
+                --no-merges `
+                --include $Path `
+                --template '{author}\n' `
+                2> $null `
+                | group `
+                | sort Count -Descending `
+                | %{$_.Name} `
+                | select -First 1 `
+        }
+
+    }
+
     Process {
-        hg log `
-            --no-merges `
-            --include $Path `
-            --template '{author}\n' `
-            2> $null `
-            | group `
-            | sort Count -Descending `
-            | %{$_.Name} `
-            | select -First 1 `
+        DetermineHgOwnerUsingLog $Path
     }
 
 }
