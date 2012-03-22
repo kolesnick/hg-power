9d5d14ec04d8bd62fcb7c5451128168fec0c45d5
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2012-03-22 21:35:46 +0200

    Hg-RemoveMissing: it no longer tries to remove all files, but now removes only files with hg status 'missing'

--- Hg-RemoveMissing.ps1
+++ Hg-RemoveMissing.ps1
@@ -1,7 +1,18 @@
 function Remove-MissingHgFiles {
 
-    hg remove `
-        --after `
+    hg status `
+        2> $null `
+        | where {$_.StartsWith('! ')} `
+        | %{$_.Remove(0, 2)} `
+        | %{
+
+            hg remove `
+                --after `
+                $_ `
+
+            write $_
+
+        }
 
 }
 
