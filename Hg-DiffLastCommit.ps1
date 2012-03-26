a10bee38ce4abf7d6e9e40e7fd6d72b54baad877
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2012-03-26 17:49:37 +0300

    Hg-DiffLastCommit: now calls Hg-Diff (implementation copy-paste removed)
    closed issue #6

--- Hg-DiffLastCommit.ps1
+++ Hg-DiffLastCommit.ps1
@@ -1,12 +1,6 @@
 function Write-LastHgCommitDiff {
 
-    $lastChangesetId = hg log `
-        --limit 1 `
-        --template '{node}' `
-        2> $null `
-
-    hg diff `
-        --change $lastChangesetId `
+    hgdiff -1
 
 }
 
