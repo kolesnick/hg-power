31c1d5f77a86cf89f1db087b2e6c22a754a9d7ee
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2012-03-26 17:02:37 +0300

    Hg-Diff: implemented for negative commit indexes and with branches support
    index -1 shows diff for latest commit, -2 for second from latest, etc.

---
+++ Hg-Diff.ps1
@@ -0,0 +1,31 @@
+function Write-HgDiff([Parameter(Mandatory=$true)] [int] $Index, [string] $Branch) {
+
+    if ($Index -gt -1) {
+        throw 'Only negative commit indexes are supported.'
+    }
+
+    if ([string]::IsNullOrEmpty($Branch)) {
+
+        $changesetId = hg log `
+            --limit (-$Index) `
+            --template '{node}\n' `
+            2> $null `
+            | select -Last 1 `
+
+    } else {
+
+        $changesetId = hg log `
+            --limit (-$Index) `
+            --branch $Branch `
+            --template '{node}\n' `
+            2> $null `
+            | select -Last 1 `
+
+    }
+
+    hg diff `
+        --change $changesetId `
+
+}
+
+Set-Alias hgdiff Write-HgDiff
