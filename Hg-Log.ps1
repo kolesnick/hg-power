1144f875c5608bb881bd45f3ef2a4416d7109e3d
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2012-03-30 02:00:18 +0300

    Hg-Log: native hg log results are now parsed (commit revision number and parent revision numbers are extracted)

--- Hg-Log.ps1
+++ Hg-Log.ps1
@@ -1,8 +1,58 @@
 function Write-HgLog {
 
+    function ParseRevisionNumber([string] $RevisionNumberChangesetIdPair) {
+        return [int] $RevisionNumberChangesetIdPair.Split(':')[0]
+    }
+
+    function ParseCommitInfo([Parameter(ValueFromPipeline=$true)] [string] $LogEntry) {
+
+        Process {
+
+            $logEntryParts = $logEntry.Split([string[]] ' <- ', [StringSplitOptions]::None)
+
+            $revision = ParseRevisionNumber($logEntryParts[0])
+
+            $parents = $logEntryParts[1].Split(' ', [StringSplitOptions]::RemoveEmptyEntries)
+
+            if ($revision -eq 0) {
+
+                # initial commit: has no parents
+                $parentA = $null
+                $parentB = $null
+
+            } else {
+
+                switch ($parents.Count) {
+                    0 {
+                        # trivial commit: previous revision is also parent one
+                        $parentA = $revision - 1
+                        $parentB = $null
+                    }
+                    1 {
+                        # parent is only one and it is explicitly specified
+                        $parentA = ParseRevisionNumber($parents[0])
+                        $parentB = $null
+                    }
+                    2 {
+                        # two parents, both explicitly specified
+                        $parentA = ParseRevisionNumber($parents[0])
+                        $parentB = ParseRevisionNumber($parents[1])
+                    }
+                }
+
+            }
+
+            return New-Object PSObject -Property @{ Revision = $revision; ParentA = $parentA; ParentB = $parentB }
+
+        }
+
+    }
+
     hg log `
         --limit 32 `
         --template '{rev}:{node|short} <- {parents}\n' `
+        2> $null `
+        | ParseCommitInfo `
 
 }
 
